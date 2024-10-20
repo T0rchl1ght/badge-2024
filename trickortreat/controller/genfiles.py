6c23e99188e99c5911469a2ed3f052b4ad8d0bea
@securelyfitz <joefitz@joefitz.net>
2024-10-20 16:38:07 -0700

    merging genfiles and flash

--- trickortreat/controller/genfiles.py
+++ trickortreat/controller/genfiles.py
@@ -6,12 +6,17 @@ import binascii
 import adafruit_rsa
 import json
 from math import ceil
+import pyudev
+import time
+import subprocess
+from threading import Thread
+import time
+import hashlib
+
 
 #this should do all the event-specific stuff
-# todo: move candies and badge count to config file somewhere?
 # todo: regenerate signatures if a new key was generated
-# todo: make sure there's space for badge ID's in signatures dictionary
-# todo: integrate with flash.py
+# todo: parameterize the file load/unload/generate
 
 candies=["Sour Patch Kids","Haribo Gummies","Smarties","Reese's Cups","Twix","Snickers"]
 badge_count=20
@@ -23,12 +28,12 @@ try:
         private_obj = json.loads(f.read())
         private_key=adafruit_rsa.PrivateKey(*private_obj["private_key_arguments"])
         public_key=adafruit_rsa.PublicKey(private_key.n, private_key.e)
-    print("Keys loaded from priv.json\n")
+    print("1. Prior keys loaded from priv.json.")
 except Exception as e:
     if input("no keys found, generate them? 'yes' to continue, anything else to quit\n") != "yes":
         exit(1)
     #remove all other config files, or set flag?
-    print("Generating new keys: ",e)
+    print("1. Generating new keys...")
     (public_key, private_key) = adafruit_rsa.newkeys(512)
     public_obj = {"public_key_arguments": [public_key.n, public_key.e]}
     private_obj = { "private_key_arguments": [private_key.n, private_key.e,
@@ -50,11 +55,11 @@ try:
     with open("seed.txt", 'r') as file:
         seed = file.read()
         random.seed(seed)
-    print("seed loaded from seed.txt\n")
+    print("2. Prior seed loaded from seed.txt.")
 except Exception as e:
     if input("no random seed found, generate one? 'yes' to continue, anything else to quit\n") != "yes":
         exit(1)
-    print("Choosing new random seed: ")
+    print("2. Choosing new random seed... ")
     seed = random.randrange(sys.maxsize)
     rng = random.Random(seed)
     f=open("seed.txt", "w")
@@ -69,11 +74,11 @@ signatures={}
 try:
     with open("candies.json", 'r') as f:
         signatures = json.loads(f.read())
-    print("candies loaded from candies.json\n")
+    print("3. Prior candies loaded from candies.json.")
 except Exception as e:
     if input("no candy signatures found, generate them? 'yes' to continue, anything else to quit\n") != "yes":
         exit(1)
-    print("generating candy signatures:")
+    print("3. Generating new candy signatures..")
     candy_count=ceil(badge_count/len(candies))
     for i in range(candy_count):
         for candy in candies:
@@ -89,21 +94,154 @@ assignments={}
 try:
     with open("assignments.json", 'r') as f:
         assignments = json.loads(f.read())
-    print("assignments loaded from assignments.json\n")
+    print("4. Prior assignments loaded from assignments.json.")
 except Exception as e:
     if input("no prior assignments found, start from scratch? 'yes' to continue, anything else to quit\n") != "yes":
         exit(1)
+    print("4. New assignments log ready...")
 
 allcandies=list(signatures.keys())
-# now, as part of flash.py write this to mountpoint/id.json
-for sn in ["1","2","3","4","5","4","3"]:
+
+print("keys, candies, and signatures loaded. Entering flashing mode. Plug in some badges!!\n")
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by('block')
+circuitpy="adafruit-circuitpython-seeeduino_xiao_rp2040-en_US-9.1.4.uf2"
+rpi_nuke="flash_nuke.uf2"
+cpy_nuke="code.py"
+gamefiles="../badge/*"
+
+# write the correct id.json file for this badge serial number to the indicated destination
+def registerserial(sn,dest):
+    # if a new serial, assign the next candy
     if sn not in assignments:
-        print("adding"+str(sn))
-        print(len(assignments))
-        assignments[sn]=allcandies[len(assignments)]
-    with open("id"+sn+".json","w") as f:
+        candyid=allcandies[len(assignments)]
+        print(sn+" now assigned to give out "+candyid)
+        assignments[sn]=candyid
+    else:
+        print(sn+" was already assigned "+assignments[sn])
+
+    # always write out to destination
+    with open(dest,"w") as f:
         f.write(json.dumps({'name': '', 'candy': assignments[sn], 'signature': signatures[assignments[sn]]}))
 
-with open("assignments.json", 'w') as f:
-    f.write(json.dumps(assignments))
+    # update our database
+    with open("assignments.json", 'w') as f:
+        f.write(json.dumps(assignments))
 
+# node is a badge that was just plugged in. Mount it and return mount point.
+def mountnode(node):
+    subprocess.run("pmount "+node,shell=True)
+    cmd='mount | grep '+node+' | cut -d " " -f 3'
+    mountpoint=str(subprocess.check_output(cmd,shell=True),'UTF-8').strip()
+    #print(node," was mounted at ",mountpoint)
+    return mountpoint
+
+# unmount the badge at this mount point
+def umountpoint(mountpoint):
+    cmd="sync -d "+mountpoint+" && pumount "+mountpoint
+    subprocess.run(cmd,shell=True)
+    #print(mountpoint, " unmounted - unplug it")
+
+# copy this file to the badge at this mount point 
+def copytodevice(file, mountpoint):
+    #print("copying ", file, " to ", mountpoint)
+    cmd="cp -Lr "+file+" "+mountpoint+"/ && sync -d "+mountpoint
+    subprocess.run(cmd,shell=True)
+
+# copy game files to a badge with circuitpython loaded
+def mkcpy(device):
+    start=time.time()
+    mountpoint=mountnode(device.device_node)
+
+    if mountpoint == "":
+        raise Exception("empty mountpoint, would delete your system!!\n")
+    sn=0
+    sn=device.get('ID_SERIAL_SHORT')
+    print(sn)
+
+    # erase the existing filesystem if any
+    # a better way might be to copy over code.py with storage.erase_filesystem()
+    cmd="rm -rf "+mountpoint+"/*"
+    subprocess.run(cmd,shell=True)
+
+    #copy game code to badge
+    copytodevice(gamefiles, mountpoint)
+
+    #write the unique id.json to the device
+    registerserial(sn,mountpoint+"/id.json")
+    
+    #clean up and report
+    umountpoint(mountpoint)
+    print("CPY #"+sn+" took "+str(time.time()-start)+"s")
+
+# copy circuitpython to a blank rpi badge
+def mkrpi(device):
+    start=time.time()
+    mountpoint=mountnode(device.device_node)
+    copytodevice(circuitpy,mountpoint)
+    umountpoint(mountpoint)
+    print("RPI took "+str(time.time()-start)+"s\n")
+
+# wipe flash on a blank rpi badge (which may still have file fragments in flash)
+def nukerpi(device):
+    start=time.time()
+    mountpoint=mountnode(device.device_node)
+    copytodevice(rpi_nuke,mountpoint)
+    umountpoint(mountpoint)
+    print("Nuking RPI took "+str(time.time()-start)+"s\n")
+
+# reboot badge in rpi uf2 mode for nukerpi
+def nukecpy(device):
+    start=time.time()
+    mountpoint=mountnode(device.device_node)
+    copytodevice(cpy_nuke,mountpoint)
+    umountpoint(mountpoint)
+    print("Nuking CPY took "+str(time.time()-start)+"si\n")
+
+
+rpicount=0
+cpycount=0
+nukemode=False
+# listen for device actions
+for device in iter(monitor.poll, None):
+    if device.action == 'add':
+        # a device has been plugged in. Get it's name and do something to it
+        label=device.get('ID_FS_LABEL')
+        if label=="RPI-RP2":
+            if nukemode:
+                # the badge booted in uf2 mode and we want to nuke it with a uf2 that will zero all of flash
+                print("Nuking flash on RPI at",device.device_node,"\n")
+                nukerpi(device)
+                nukemode=False
+                continue
+            # the badge is in uf2 mode, and we're just going to fork a thread to flash it with circuitpython
+            rpicount+=1
+            print("RPI #",str(rpicount),"\n")
+            Thread(target=mkrpi, args=(device,)).start()
+        if label=="CIRCUITPY":
+            if nukemode:
+                # badge running circuitpython and we want to reboot it in uf2 mode to nuke
+                print("Nuking flash on CPY at",device.device_node,"\n")
+                nukecpy(device)
+                #need to nuke RP2 after this
+                #nukemode=False
+                continue
+            # a badge running circutpython is plugged in - fork a thread to copy game files over
+            cpycount+=1
+            print("CPY #",str(cpycount))
+            Thread(target=mkcpy, args=(device,)).start()
+        if label=="CLEAR":
+            # plugging in a usb drive with the volume label "CLEAR" will pumount all pmounted devices
+            cmd="ls -lah /media/"
+            subprocess.run(cmd,shell=True)
+            print("unmounting\n")
+            for filename in os.scandir("/media/"):
+                umountpoint(filename.path)
+                print(filename)
+            subprocess.run(cmd,shell=True)
+        if label=="NUKE":
+            # plugging in a usb drive with the volume label "NUKE" will completely zero the flash of the next badge found
+            nukemode=True
+            print("Nuke mode enabled- next badge will get flash wiped first\n")
