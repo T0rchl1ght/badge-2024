531959ca8476816d9a3a3064cd74a1f926dc9131
@securelyfitz <joefitz@joefitz.net>
2024-10-19 23:42:21 -0700

    adding keyboard entry; cleaning up home.py OOBE messages

--- trickortreat/badge/home.py
+++ trickortreat/badge/home.py
@@ -4,19 +4,12 @@
 # import random
 # from ssd1306_ui import box
 # from adafruit_display_text.label import Label
-import time
-import json
-import adafruit_rsa
 
 # BLACK=0x000000
 # WHITE=0xFFFFFF
-###############################################################
 
-#####################################################################################
 # this class manages the home display and navigation
 # it also presents the new user process when no name is defined
-
-
 class home:
     def __init__(self, l_disp, dpad):
         # self.group=group
@@ -24,8 +17,9 @@ class home:
         self.game = None
         self.thanks = False  # we have 2 main displays, directions and thanks
         self.disp = l_disp
+        self.event_name = "BSidesPDX '24"
 
-        self.disp.setHeader("BSidesPDX 2024")
+        self.disp.setHeader(self.event_name)
 
         # ToDo: breakout badge setup into it's own thing, should be checked in code.py
         # get the username from file. This duplicates the work done by game.read_name()
@@ -33,11 +27,7 @@ class home:
         try:
             with open("data/myname.txt", "r") as file:
                 name = file.readline().rstrip()
-                self.disp.setText(
-                    ["Trick\tor\tTreat", "Welcome Back,", name], align="c"
-                )
         except OSError as e:
-            print("Error on home.py line 32")
             print(e)
             name = ""
 
@@ -45,132 +35,148 @@ class home:
         # ToDo: Move to config?
         # ToDo: Break out the instructions message into a config item
         if name == "":
-            while True:
-                self.dpad.update()
-                if self.dpad.x.fell:
-                    break
-                self.disp.setText(["Trick", "Or", "Treat!"], align="c")
-                time.sleep(0.01)
-
-            while True:
-                self.dpad.update()
-                if self.dpad.x.fell:
-                    break
-                self.disp.setText(
-                    [
-                        "Meet people & trade",
-                        "clues to attribute",
-                        "the attack",
-                        "You don't have",
-                        "a handle yet!",
-                        "Your handle will",
-                        "be shared when you",
-                        "trade in the game.",
-                    ]
-                )
-                time.sleep(0.01)
+            self.showAndWait("Welcome to BSides PDX Trick or Treat! Press any button.")
+            self.showAndWait(
+                ["5-way       (^)", "d-pad    (<)(x)(>)", "buttons:    (v)"]
+            )
+            self.showAndWait(["Pick a", "name/handle", "to share"])
+            self.showAndWait(["(<)(>)(^)(v) to", "highlight a letter", "(x) to select"])
+            self.showAndWait(['"," in the bottom', "left to", "change keyboard"])
+            self.showAndWait(
+                ['"<" Backspace and', '">" Done, both', "in the bottom right"]
+            )
 
             # nameEntry blocks, not fixing
             name = self.nameEntry()
 
-            while True:
-                self.dpad.update()
-                if self.dpad.x.fell:
-                    break
-                self.disp.setText(
-                    [
-                        "Welcome to the game",
-                        name,
-                        "press '^' & point @",
-                        "another to trade",
-                        "contact & clues!",
-                        "press '<' to get to",
-                        "settings and a list",
-                        "of alibis",
-                        "press '>' to get to",
-                        "your clue collection",
-                        "" "Collect enough",
-                        "clues to figure",
-                        "out what happened",
-                        "Have fun!",
-                    ]
-                )
-                time.sleep(0.01)
+            self.showAndWait(
+                ["Welcome " + name + "!", "(^) to trade", "candy and handles"]
+            )
+            self.showAndWait(
+                ["(<) to see friends", "(<)(<) to settings", "(>) to see candy!"]
+            )
+            self.showAndWait(
+                ["Collect 5+ to redeem", "digital candy for", "the real thing!"]
+            )
+
+    def showAndWait(self, text):
+        self.disp.setText(text, align="c")
+        self.dpad.update()
+        while not self.dpad.x.fell:
+            self.dpad.update()
 
     # show a string and handle text input
     def nameEntry(self):
-        instructions = ["Enter your handle", "Press to save"]
+        keyboards = [
+            ["Q W E R T Y U I O P ", "A S D F G H J K L   ", ", Z X C V B N M < > "],
+            ["q w e r t y u i o p ", "a s d f g h j k l   ", ", z x c v b n m < > "],
+            ["1 2 3 4 5 6 7 8 9 0 ", "! @ # $ % & * ( )   ", ",   ? [ ] + - . < > "],
+        ]
+        kbindex = 0
+        keyboard = keyboards[0]
         player_name = [" "] * 20  # max name length
-        chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()'<>.?/|\\{}[] "
-        cindex = 0
+        kx = 0
+        ky = 0
         nindex = 0
-        player_name[nindex] = chars[cindex]
-        self.disp.setTextCursor(instructions + ["".join(player_name)], (nindex, 2))
+        player_name[nindex] = keyboard[ky][kx]
+        self.disp.setHeader("handle:" + "".join(player_name))
+        self.disp.setTextCursor(keyboard, (kx, ky))
+
         while True:
             self.dpad.update()
             if self.dpad.u.fell:
                 # u increments through the character set for names
-                cindex = (cindex + 1) % len(chars)
-                player_name[nindex] = chars[cindex]
-                self.disp.setTextCursor(
-                    instructions + ["".join(player_name)], (nindex, 2)
-                )
+                ky = (ky - 1) % len(keyboard)
+            #                player_name[nindex] = keyboard[ky][kx]
+            #                self.disp.setHeader("handle:"+"".join(player_name))
+            #                self.disp.setTextCursor(keyboard, (kx,ky))
             elif self.dpad.d.fell:
                 # d decrements through the character set for names
-                cindex = (cindex - 1) % len(chars)
-                player_name[nindex] = chars[cindex]
-                self.disp.setTextCursor(
-                    instructions + ["".join(player_name)], (nindex, 2)
-                )
+                ky = (ky + 1) % len(keyboard)
+            #                player_name[nindex] = keyboard[ky][kx]
+            #                self.disp.setHeader("handle:"+"".join(player_name))
+            #                self.disp.setTextCursor(keyboard, (kx,ky))
             elif self.dpad.l.fell:
                 # l moves to the previous char
-                nindex = nindex - 1 if nindex > 0 else 0
-                cindex = chars.find(player_name[nindex])
-                self.disp.setTextCursor(
-                    instructions + ["".join(player_name)], (nindex, 2)
-                )
+                kx = (kx - 2) % len(keyboard[ky])
             elif self.dpad.r.fell:
                 # r moves to the next char
-                nindex = (
-                    nindex + 1
-                    if nindex < len(player_name) - 1
-                    else len(player_name) - 1
-                )
-                # move cindex to existing character at slot
-                cindex = chars.find(player_name[nindex])
-                self.disp.setTextCursor(
-                    instructions + ["".join(player_name)], (nindex, 2)
-                )
+                kx = (kx + 2) % len(keyboard[ky])
+            #                player_name[nindex] = keyboard[ky][kx]
+            #                self.disp.setHeader("handle:"+"".join(player_name))
+            #                self.disp.setTextCursor(keyboard, (kx,ky))
             elif self.dpad.x.fell:
-                # x means we're done. Verify and resume.
-                self.disp.setText(
-                    ["Saving your handle:", str.rstrip("".join(player_name))]
-                )
-                self.disp.setText("Saving handle")
-                try:
-                    with open("data/myname.txt", "w") as file:
-                        file.write("".join(player_name))
-                except OSError as e:
-                    print(e)
-                print("player_name: {}".format("".join(player_name)))
-                return str.rstrip("".join(player_name))
-            time.sleep(0.01)
+                # "," means cycle keyboards
+                if keyboard[ky][kx] == ",":
+                    kbindex = (kbindex + 1) % len(keyboards)
+                    keyboard = keyboards[kbindex]
+                #                    self.disp.setTextCursor(keyboard, (kx,ky))
+                # "<" means backspace
+                elif keyboard[ky][kx] == "<" and nindex > 0:
+                    player_name[nindex] = " "
+                    nindex = nindex - 1
+                #                    player_name[nindex] = keyboard[ky][kx]
+                #                    self.disp.setHeader("handle:"+"".join(player_name))
+                # ">" means done.
+                elif keyboard[ky][kx] == ">":
+                    player_name[nindex] = "\n"
+                    self.disp.setText("Saving handle")
+                    # todo:add confirmation
+                    try:
+                        with open("data/myname.txt", "w") as file:
+                            file.write("".join(player_name))
+                    except OSError as e:
+                        print(e)
+                    print("player_name: {}".format("".join(player_name)))
+                    return str.rstrip("".join(player_name))
+                # any other letter: append to name
+                else:
+                    nindex = nindex + 1
+            else:
+                continue
+            # refresh
+            player_name[nindex] = keyboard[ky][kx]
+            self.disp.setHeader("handle:" + "".join(player_name))
+            self.disp.setTextCursor(keyboard, (kx, ky))
+
+    #            time.sleep(0.01)
 
     def update(self):
         # show contents, process keypresses
         # self.disp.hidden=False
-        self.disp.setHeader("BSidesPDX 2024")
+        self.disp.setHeader(self.event_name)
 
-        # this is causing oom!
-        if self.disp.setText(self.game.solution_string):
-            if self.dpad.u.fell:
-                return "trade"
-            elif self.dpad.d.fell:
-                return "sleep"
-        if self.dpad.l.fell:
-            return "alibis"
-        elif self.dpad.r.fell:
-            return "clues"
-        elif self.dpad.x.fell:
-            self.thanks = not self.thanks
+        if self.thanks:
+            self.disp.setHeader("Sponsors")
+            if self.disp.setText(
+                [
+                    "Thank you to our",
+                    "Silver Sponsors:",
+                    "BPM",
+                    "SecuringHardware.com",
+                ]
+            ):
+                if self.dpad.u.fell:
+                    return "trade"
+                elif self.dpad.d.fell:
+                    return "sleep"
+            if self.dpad.x.fell:
+                self.thanks = not self.thanks
+            elif self.dpad.l.fell:
+                return "alibis"
+            elif self.dpad.r.fell:
+                return "clues"
+        else:
+            # this is causing oom!
+            if self.disp.setText(self.game.solution_string):
+                if self.dpad.u.fell:
+                    return "trade"
+                elif self.dpad.d.fell:
+                    return "sleep"
+            if self.dpad.l.fell:
+                return "alibis"
+            elif self.dpad.r.fell:
+                return "clues"
+            elif self.dpad.x.fell:
+                self.thanks = not self.thanks
         return "home"
