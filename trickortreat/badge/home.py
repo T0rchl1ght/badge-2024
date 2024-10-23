b95e7ad3a6120711a710c9805e529d78ed836ee7
@securelyfitz <joefitz@joefitz.net>
2024-10-22 23:11:53 -0700

    trimming down home screen, sponsors to settings, display user info

--- trickortreat/badge/home.py
+++ trickortreat/badge/home.py
@@ -11,53 +11,37 @@
 # this class manages the home display and navigation
 # it also presents the new user process when no name is defined
 class home:
-    def __init__(self, l_disp, dpad):
+    def __init__(self, l_disp, dpad, game):
         # self.group=group
         self.dpad = dpad
-        self.game = None
+        self.game = game
         self.thanks = False  # we have 2 main displays, directions and thanks
         self.disp = l_disp
         self.event_name = "BSidesPDX '24"
 
         self.disp.setHeader(self.event_name)
 
-        # ToDo: breakout badge setup into it's own thing, should be checked in code.py
-        # get the username from file. This duplicates the work done by game.read_name()
-        # but i'd rather add file i/o here than add display management there
-        try:
-            with open("data/myname.txt", "r") as file:
-                name = file.readline().rstrip()
-        except OSError as e:
-            print(e)
-            name = ""
-
-        # if there's no file or it's blank, give some instructions and ask for a name
-        # ToDo: Move to config?
         # ToDo: Break out the instructions message into a config item
-        if name == "":
+        if self.game.myname == "":
             self.showAndWait("Welcome to BSides PDX Trick or Treat! Press any button.")
-            self.showAndWait(
-                ["5-way       (^)", "d-pad    (<)(x)(>)", "buttons:    (v)"]
-            )
+            self.showAndWait( ["5-way       (^)", "d-pad    (<)(x)(>)", "buttons:    (v)"])
             self.showAndWait(["Pick a", "name/handle", "to share"])
             self.showAndWait(["(<)(>)(^)(v) to", "highlight a letter", "(x) to select"])
             self.showAndWait(['"," in the bottom', "left to", "change keyboard"])
-            self.showAndWait(
-                ['"<" Backspace and', '">" Done, both', "in the bottom right"]
-            )
+            self.showAndWait( ['"<" Backspace and', '">" Done, both', "in the bottom right"])
 
             # nameEntry blocks, not fixing
-            name = self.nameEntry()
+            self.game.set_name(self.nameEntry())
 
-            self.showAndWait(
-                ["Welcome " + name + "!", "(^) to trade", "candy and handles"]
-            )
-            self.showAndWait(
-                ["(<) to see friends", "(<)(<) to settings", "(>) to see candy!"]
-            )
-            self.showAndWait(
-                ["Collect 5+ to redeem", "digital candy for", "the real thing!"]
-            )
+            self.showAndWait( ["Welcome " + self.game.myname + "!", "(^) to trade", "candy and handles"])
+            self.showAndWait( ["(<) to see friends", "(<)(<) to settings", "(>) to see candy!"])
+            self.showAndWait( ["Collect 5+ to redeem", "digital candy for", "the real thing!"])
+
+        header=self.event_name
+        body=["^ trade","< friends  candy >", "v sleep (x) more"]
+        self.disp.setHeader(header)
+        self.disp.setText(body)
+
 
     def showAndWait(self, text):
         self.disp.setText(text, align="c")
@@ -110,11 +94,6 @@ class home:
                     player_name[nindex] = "\n"
                     self.disp.setText("Saving handle")
                     # todo:add confirmation
-                    try:
-                        with open("data/myname.txt", "w") as file:
-                            file.write("".join(player_name))
-                    except OSError as e:
-                        print(e)
                     print("player_name: {}".format("".join(player_name)))
                     return str.rstrip("".join(player_name))
                 # any other letter: append to name
@@ -131,15 +110,11 @@ class home:
         # show contents, process keypresses
         # self.disp.hidden=False
         header=self.event_name
-        body=""
+        body=["^ trade","< friends  candy >", "v sleep (x) more"]
 
         if self.thanks:
             header="Sponsors"
-            body=   ["Thank you to our",
-                    "Silver Sponsors:",
-                    "BPM",
-                    "SecuringHardware.com",
-                    ]
+            body=self.game.myname+" is giving out "+self.game.mycandy
             
         self.disp.setHeader(header)
         if self.disp.setText(body):
